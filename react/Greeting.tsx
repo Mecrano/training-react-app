bffb1140c23fe1e19f053a25a6834a7c22f9e9c4
Edwin Obando <edwin.alex1995@gmail.com>
2021-06-04 17:38:06 -0500

    implement basic greeting

--- react/Greeting.tsx
+++ react/Greeting.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'vtex.styleguide'
+import { useLazyQuery } from 'react-apollo'
+
+import GET_DOCUMENTS from './graphql/query.getDocuments.gql'
 
 type Props = {
-  name: string
+  email: string
 }
 
-function Greeting({ name }: Props) {
-  return <div>Hey, {name}</div>
+function Greeting({ email }: Props) {
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [loadGreeting, { called, loading, error, data }] = useLazyQuery(
+    GET_DOCUMENTS
+  )
+
+  const getUser = () => {
+    loadGreeting({
+      variables: {
+        acronym: 'CL',
+        fields: ['lastName'],
+        where: `email=${email}`,
+      },
+    })
+  }
+
+  const getFieldDef = (fields: any, nameField: any) => {
+    const field = fields?.documents[0].fields.find(
+      ({ key }: any) => key === nameField
+    )
+
+    return field?.value
+  }
+
+  useEffect(() => {
+    if (!loading && called) {
+      if (error) {
+        console.error('Error', error)
+      } else {
+        // eslint-disable-next-line no-console
+        console.log(data)
+        const nombre: string = getFieldDef(data, 'lastName')
+        const apellido: string = getFieldDef(data, 'lastName')
+
+        if (nombre) {
+          setFirstName(nombre)
+        }
+
+        if (apellido) {
+          setLastName(apellido)
+        }
+      }
+    }
+  }, [called, loading, error, data])
+
+  return (
+    <>
+      {firstName && lastName ? (
+        <div>Hola, {`${firstName} ${lastName}`}</div>
+      ) : (
+        <p>Sin Data</p>
+      )}
+      <Button variation="primary" onClick={() => getUser()}>
+        Consultar
+      </Button>
+    </>
+  )
 }
 
 export default Greeting
